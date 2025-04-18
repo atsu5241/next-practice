d108b80f738791a8fa2d9158b8f8d807e2c4e519
小西敦也 <atsu524128@gmail.com>
2025-04-18 11:31:02 +0900

    second deploy

--- src/app/api/purchases/[userId]/route.ts
+++ src/app/api/purchases/[userId]/route.ts
@@ -1,13 +1,12 @@
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import prisma from '@/app/lib/prisma';
 
 // 購入履歴検索API
 export async function GET(
-  request: Request,
-  { params }: { params: { userId: string } }
+  request: NextRequest,
+  context: { params: { userId: string } }
 ) {
-  const userId = params.userId;
-
+  const userId = context.params.userId;
   try {
     const purchases = await prisma.purchase.findMany({
       where: { userId: userId },
